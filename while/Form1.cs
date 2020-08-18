caddb0a9ee6b7266af82b25ea6b1a37909026acf
HazelicEyes <henriquepromail9863@gmail.com>
2020-08-18 03:58:31 -0300

    Add project files.

---
+++ while/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace @while
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            int numero = 1;
+            while (numero <= 100)
+            {
+                listBox1.Items.Add(numero);
+                numero = numero + 1;
+
+                /* declara variavel inteiro, variavel iniciada com 1
+                 * enquanto o numero for menor ou igual a 100 listbox sera preenchido
+                 * expressão = numero + numero1 é o incremento */
+            }
+        }
+    }
+}
